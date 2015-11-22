1caba5915b15e8e66c87b49f0b2ee2b97ad63bab
THenry14 <wojciech.szymczyk14@gmail.com>
2015-11-22 19:08:41 +0100

    Fix reading contacts and add dialing

--- app/src/main/java/nowim/speechtotext/MainActivity.java
+++ app/src/main/java/nowim/speechtotext/MainActivity.java
@@ -1,9 +1,8 @@
 package nowim.speechtotext;
 /*
 Wojciech Szymczyk, Michał Czerwień
-Virtual Personal Assistant, ver 0.2
+Virtual Personal Assistant, ver 0.3
 Api level: 17
-
 
  */
 import android.content.ContentResolver;
@@ -12,18 +11,20 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 //import android.app.Activity;                          //probably will use it for working in widget-like fashion as messenger and in background
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;                 //now only for recognition, later on let's use speech.RecognizerResultsIntent or speech* to obtain and display the results of a search
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.MenuItem;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,8 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton ButtonToRecord;                 // initialize button
     private TextView RecognisedText;                    // initialize text area
     About about = new About();
-    // Methods read = new Methods();
-    public TextView textDetail;
+    String phone = new String();
+    String name = new String();
+    String id = new String();
+    String number = new String();
+    String nameOfContact = new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +46,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);      // view declared in xml, You can change the layout there
 
         RecognisedText = (TextView) findViewById(R.id.Text);
-        ButtonToRecord = (ImageButton) findViewById(R.id.ButtonToRecord);
         about.AboutText = (TextView) findViewById(R.id.Text);
-        //read.
-        textDetail = (TextView) findViewById(R.id.Text);
-
+        ButtonToRecord = (ImageButton) findViewById(R.id.ButtonToRecord);
 
         ButtonToRecord.setOnClickListener(new View.OnClickListener() {      // creation of layout, found by ID
 
@@ -55,11 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(
                         RecognizerIntent.ACTION_RECOGNIZE_SPEECH);          // after pressing button, perform the action of recognition
-                //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
 
                 try {                                                       // if speech recognition not supported, exception is being thrown
                     startActivityForResult(intent, RESULT_SPEECH);
                     RecognisedText.setText("");
+
                 } catch (ActivityNotFoundException a) {
                     Toast t = Toast.makeText(getApplicationContext(),
                             "Device doesn't support speech recognition",
@@ -72,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {                             // we can probably get rid of this, but for now we have a simple menu with settings, which does nothing. Here may be exit button implemented
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -87,54 +87,67 @@ public class MainActivity extends AppCompatActivity {
 
                     ArrayList<String> text = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    RecognisedText.setText("");
-                    RecognisedText.setText(text.get(0));
+
+                    recognise(text);
+                    readContacts(nameOfContact);
+                    makeCall(number);
+
                 }
                 break;
             }
-
         }
     }
 
-    public void readContacts () {
-        textDetail.setText("");
-        StringBuffer sb = new StringBuffer();
-        sb.append("......Contact Details.....");
+    public String recognise( ArrayList text){
+
+        String obj = (String) text.get(0);
+        text.clear();
+        text.add(obj);
+        nameOfContact = TextUtils.join(" ", text);
+        // RecognisedText.setText(nameOfContact + " View from recognised field" + "\n");  //FOR LOGGING
+
+        return nameOfContact;
+    }
+
+    public String readContacts(String nameOfContact) {
+        Map<String, String> book = new HashMap<String, String>();
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        String phone = null;
+            if (cur.getCount() > 0) {
+                while (cur.moveToNext()) {
+                    id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
 
-
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
-                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                    sb.append("\n Contact Name:" + name);
-                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        phone = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        sb.append("\n Phone number:" + phone);
+                        Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            phone = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            book.put(name, phone);
+                        }
+                        pCur.close();
                     }
-                    pCur.close();
                 }
-                sb.append("\n........................................");
             }
-            textDetail.setText(sb);
+            number = book.get(nameOfContact);
+            //RecognisedText.setText(number + " view from readContacts field" + "\n"); //FOR LOGGING
 
+        return number;
         }
-        //coś tu sie odpierdoliło.
-        // public void makeCall () {
 
-        //    if (RecognisedText.getText().toString().contains(name)) {
-        //        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        //        callIntent.setData(Uri.parse(phone));
-        //        startActivity(callIntent);
+    public void makeCall (String number) {
+        if(number != null) {
+                String numberToCall = "tel:" + number.trim();
+                Uri Call = Uri.parse(numberToCall);
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Call);
+                startActivity(callIntent);
+            }
+        else {
+            Toast t = Toast.makeText(getApplicationContext(),
+                    "No such contact in your phonebook", // later can add voice saying this and asking if you can add contact
+                    Toast.LENGTH_SHORT);
+                t.show();
+            }
     }
-    //}
-//}
-
 
 
     @Override
@@ -145,17 +158,19 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
 
+            case R.id.action_scenarios:
+
+                //possible scenarios here
+
+                return true;
+
             case R.id.action_settings:
 
-                //FOR TEST PURPOSES FOR NOW !!!!!! LATER ON WILL IMPLEMENT SETTINGS MENU
-
-                //read.
-                readContacts();
+                //later on
 
                 return true;
 
             case R.id.action_about:
-
 
                 about.AboutText();
 
@@ -173,6 +188,5 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
 }
--- app/src/main/java/nowim/speechtotext/Methods.java
+++ app/src/main/java/nowim/speechtotext/Methods.java
@@ -8,55 +8,9 @@ import android.widget.TextView;
 
 public class Methods extends AppCompatActivity {
 
-    //public TextView textDetail;
 
     public static void meth(){
 
     }
-/*  
 
-    Stefan, te metody przeniesione do pliku głównego powinny być właśnie tu - jednak tak jak mówiłem, null pointer exception się wywala
-
-    public void readContacts () {
-        textDetail.setText("");
-        StringBuffer sb = new StringBuffer();
-        sb.append("......Contact Details.....");
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        String phone = null;
-
-
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
-                String id = cur.getString(cur .getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                if (Integer.parseInt(cur.getString(cur .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
-                {
-                    sb.append("\n Contact Name:" + name);
-                    Cursor pCur = cr.query( ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[] { id }, null);
-                    while (pCur.moveToNext()) {
-                        phone = pCur .getString(pCur .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        sb.append("\n Phone number:" + phone);
-                    }
-                    pCur.close();}
-                sb.append("\n........................................"); } textDetail.setText(sb);
-
-        }
-
-
-    }
-
-*/
-
- /*   public void makeCall () {
-        String personToCall = RecognisedText;
-
-                if (RecognisedText.getText().toString().contains(name)) {
-                    Intent callIntent = new Intent(Intent.ACTION_CALL);
-                    callIntent.setData(Uri.parse(phone));
-                    startActivity(callIntent)
-                }
-    }
-*/
 }
